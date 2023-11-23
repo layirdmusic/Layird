0b8ea2d80b248970412af3cdfc1a76cbe89cedcc
Lejon Lewis <lejonlewis@Lejons-MacBook-Pro.local>
2023-11-23 06:54:44 -0500

    update

---
+++ src/Components/StickyPlayer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function StickyPlayer(){
+
+
+    return(
+        <>
+        <div className="musicplayer-backing">
+            <div className="song-details-container">
+            
+            </div>
+            
+            <div className="player-nav-container">
+
+            </div>
+
+            <div className="player-volume-container">
+
+            </div>
+
+        </div>
+
+        </>
+    );
+};
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StickyPlayer from "../Components/StickyPlayer";
+
+export default function Home(){
+
+
+    return(
+        <>
+            <StickyPlayer />
+        </>
+    )
+};
